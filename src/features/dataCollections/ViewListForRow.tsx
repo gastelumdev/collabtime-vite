2d05e7957391162299c2de853e7885174b61a250
Omar Gastelum <ogastelum@collabtime.co>
2024-11-11 10:51:49 -0800

    Add calendar to planner app

--- src/features/dataCollections/ViewListForRow.tsx
+++ src/features/dataCollections/ViewListForRow.tsx
@@ -32,6 +32,11 @@ import { emptyPermissions } from '../workspaces/UserGroups';
 import { io } from 'socket.io-client';
 import DataCollection from './DataCollection';
 
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
 const ViewListForRow = ({}: { allowed?: boolean }) => {
     // const [data, setData] = useState<TDataCollection[]>(dataCollections);
     const navigate = useNavigate();
@@ -132,10 +137,17 @@ const ViewListForRow = ({}: { allowed?: boolean }) => {
                                                           return dataCollection !== null && viewDC ? (
                                                               <Box key={dataCollection.name} mb={'30px'}>
                                                                   <Text fontSize={'xl'}>{`${dataCollection.name} ${dataCollection.appType}`}</Text>
-                                                                  <Tabs>
+                                                                  <Tabs
+                                                                      onChange={() => {
+                                                                          setTimeout(function () {
+                                                                              window.dispatchEvent(new Event('resize'));
+                                                                          }, 1);
+                                                                      }}
+                                                                  >
                                                                       <TabList>
                                                                           <Tab>Grid</Tab>
                                                                           <Tab>List</Tab>
+                                                                          <Tab>Calendar</Tab>
                                                                       </TabList>
                                                                       {/* <TabPanels>
                                                                           <TabPanel>
@@ -187,6 +199,52 @@ const ViewListForRow = ({}: { allowed?: boolean }) => {
     );
 };
 
+function renderEventContent(eventInfo: any) {
+    return (
+        <Box pl={'6px'} overflow={'hidden'}>
+            <Flex>
+                <Box mr={'7px'}>
+                    <Text fontWeight={'bold'}>{eventInfo.timeText}</Text>
+                </Box>
+                <Box>
+                    <Text>{eventInfo.event.title}</Text>
+                </Box>
+            </Flex>
+        </Box>
+    );
+}
+
+// const events = [
+//     {
+//         id: 0,
+//         title: 'Project 1',
+//         allDay: true,
+//         start: new Date(2024, 10, 0),
+//         end: new Date(2024, 10, 7),
+//     },
+//     {
+//         id: 1,
+//         title: 'Project 1',
+//         allDay: true,
+//         start: new Date(2024, 10, 0),
+//         end: null,
+//     },
+
+//     {
+//         id: 2,
+//         title: 'DTS STARTS',
+//         start: new Date(2016, 2, 13, 0, 0, 0),
+//         end: new Date(2016, 2, 20, 0, 0, 0),
+//     },
+
+//     {
+//         id: 3,
+//         title: 'DTS ENDS',
+//         start: new Date(2016, 10, 6, 0, 0, 0),
+//         end: new Date(2016, 10, 13, 0, 0, 0),
+//     },
+// ];
+
 const PlannerApp = ({
     dataCollection,
     dataCollectionId,
@@ -211,7 +269,11 @@ const PlannerApp = ({
         sortBy: 'createdAt',
         filters: JSON.stringify(dataCollection.filters),
     });
-    useEffect(() => {}, [rowsData]);
+    useEffect(() => {
+        setTimeout(function () {
+            window.dispatchEvent(new Event('resize'));
+        }, 1000);
+    }, [rowsData]);
     return (
         <TabPanels>
             <TabPanel>
@@ -236,6 +298,20 @@ const PlannerApp = ({
                         rowsData={rowsData}
                         refetchRowsForApp={refetch}
                     />
+                </Box>
+            </TabPanel>
+            <TabPanel>
+                <Box>
+                    <Container maxW={'6xl'}>
+                        <CalendarPanel
+                            dataCollection={dataCollection}
+                            dataCollectionId={dataCollection._id as string}
+                            appModel={dataCollection.appModel}
+                            userGroup={userGroup}
+                            rowsData={rowsData}
+                            refetchRowsForApp={refetch}
+                        />
+                    </Container>
                 </Box>
             </TabPanel>
         </TabPanels>
@@ -407,6 +483,50 @@ const OneDataCollection = ({
             appModel={appModel}
             dataCollectionPermissions={dataCollectionPermissions}
             refetchRowsForApp={refetchRowsForApp}
+        />
+    );
+};
+
+const CalendarPanel = ({
+    rowsData,
+}: {
+    dataCollection: any;
+    dataCollectionId?: string;
+    appModel: string;
+    userGroup: any;
+    rowsData: any;
+    refetchRowsForApp: any;
+}) => {
+    useEffect(() => {
+        setTimeout(function () {
+            window.dispatchEvent(new Event('resize'));
+        }, 1000);
+    }, [rowsData]);
+
+    const [events, setEvents] = useState([]);
+
+    useEffect(() => {
+        if (rowsData !== undefined) {
+            setEvents(
+                rowsData.map((row: any) => {
+                    return { title: row.values['todo'], start: row.values['start_date'], end: row.values['due_date'] };
+                })
+            );
+        }
+    }, [rowsData]);
+    return (
+        <FullCalendar
+            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+            initialView="dayGridMonth"
+            weekends={false}
+            // dateClick={(e) => handleDateClick(e)}
+            events={events}
+            eventContent={renderEventContent}
+            headerToolbar={{
+                left: 'prev,next',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,timeGridDay', // user can switch between the two
+            }}
         />
     );
 };
